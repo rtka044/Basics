23c6a0e2bcde7605e2f9eca42d43b8c2dec3e0c0
Ritika <rtka044@gmail.com>
2026-08-18 21:32:32 +0530

    initial commit

---
+++ pro.js
@@ -0,0 +1,16 @@
+//Write a program to check whether a given number is positive, negative, or zero using if-else.
+let a = Number(prompt("Enter number:"));
+if (a>0){
+    console.log("number is positive")
+}
+else if (a<0){
+    console.log("number is negative")
+}
+else{
+    console.log("number is zero")
+}
+
+
+
+
+
